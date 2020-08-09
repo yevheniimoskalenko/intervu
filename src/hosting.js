c12860da02fdc8e1ef2b7f73d9f60f859cd6b5cb
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-08-09 22:18:58 +0300

    hosting

---
+++ src/hosting.js
@@ -0,0 +1,39 @@
+// console.log(sum(1,2)) // безразницы когда ты будешь вызивать функцию, оно будет рабоатьт
+
+// function sum(a,b){
+//     return a+b
+// }
+// console.log(sum(1,2)) //выводит нормально, функц вызвана позже
+
+
+// console.log(i) // она еще не определена, но он уже существует и выдает значниее
+
+// var i = 2
+
+// console.log(i)
+
+
+// console.log(i) // не можем получить доступ, перед ее инециализацией
+
+// let i = 2
+
+// console.log(i)
+
+// console.log(i) // не можем получить доступ, перед ее инециализацией
+
+// const  i = 2
+
+// console.log(i)
+
+
+// function Expression && function Declaretion
+// console.log(squure(25)) // function Declaretion будет работать нормально можно выживать где угодно
+// function squure(params) {
+//     return params**2
+// }
+
+// const squure = function (params) {
+//     return params**2
+// }
+
+// console.log(squure(25)) // function Expression будет работать тьлько после создание функции
